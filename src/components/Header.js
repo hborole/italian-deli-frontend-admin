6fcf22e57128db2b1e906851758a6f55edbae225
Himanshu Borole <himanshu.borole@gmail.com>
2022-09-22 10:37:29 +0530

    Init commit

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Header() {
+  return (
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+        sticky="top"
+      >
+        <Container>
+          <Navbar.Brand href="/">Italian Deli</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/shop">Shop</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
---
+++ src/pages/NoPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NoPage() {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      Page Not Found!
+    </div>
+  );
+}
